d5c0f722d3e6e56b6bef7443eb69f9d3b59d46ff
pde-rent <paul@astrolab.fi>
2024-05-04 12:11:02 +0200

    redis+tdengine dockerfile updated

--- test.bash
+++ test.bash
@@ -2,4 +2,16 @@
 source .env.test
 
 docker build -f Dockerfile.dbs -t collector-dbs .
-docker run -d -p ${KDB_PORT}:${KDB_PORT} -p ${REDIS_PORT}:${REDIS_PORT} --env-file .env.test collector-dbs
+
+# Check if the container is already running
+if docker ps -a --format '{{.Names}}' | grep -q "^${DB_CONTAINER}$"; then
+    echo "Container $DB_CONTAINER already exists, stopping and deleting..."
+    # Stop the container if it's running
+    docker stop $DB_CONTAINER
+    # Remove the container
+    docker rm $DB_CONTAINER
+fi
+
+# Run the new container instance
+echo "Starting a new container $DB_CONTAINER..."
+docker run -d --name $DB_CONTAINER collector-dbs
